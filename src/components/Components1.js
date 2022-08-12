f304df501944a6ae8f697c2d3a6d66b1bb96976a
Anupam0173 <anupam.kushwaha@thinkitive.com>
2022-08-13 05:01:27 +0530

    props concept

---
+++ src/components/Components1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Compon1() {
+  return (
+    <div>components1</div>
+  )
+}
+    
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Compon1 from './components/Components1'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App name="anupam" />
+    <App name="abc" />
+    <App name="anuj" />
+    <App name="hero" />
+    <Compon1 />
   </React.StrictMode>
 );
 
